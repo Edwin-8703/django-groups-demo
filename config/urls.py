4ba56c31374459e199b04cbb3e1db4f0b851eca8
Eddy <Eddy@Eddys-iMac.local>
2026-03-06 12:23:51 +0300

    redirect root to login

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from accounts import views
 
 urlpatterns = [
+    path('',            lambda request: redirect('login')),
     path('admin/',     admin.site.urls),
     path('login/',     auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/',    auth_views.LogoutView.as_view(), name='logout'),
