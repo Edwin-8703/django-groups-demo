2500b5c9f1666ddd8cf77b5d9d4f92a7a244518e
Eddy <Eddy@Eddys-iMac.local>
2026-03-09 11:09:58 +0300

    use DATABASE_URL for PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,8 @@ TEMPLATES = [{
 }]
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
     )
 }
 
